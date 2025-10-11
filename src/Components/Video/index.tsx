78c02c5a64f3f63250ef753e05c258255a8236ba
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-10-11 08:42:08 -0300

    feat: create Video component with style

---
+++ src/Components/Video/index.tsx
@@ -0,0 +1,45 @@
+import type { CSSProperties } from "react";
+
+import styles from "./styles.module.css"
+
+type Props = {
+  videoId?: string;
+  width?: CSSProperties["width"]; // <= em vez de string
+  autoplayMuted?: boolean;
+  controls?: boolean;
+};
+
+export function Videoo({
+  videoId = "zXvqAr-oraM",
+  width = "50%",
+  autoplayMuted = true,
+  controls = true,
+}: Props) {
+  const params = new URLSearchParams({
+    // autoplay: autoplayMuted ? "1" : "0",
+    mute: autoplayMuted ? "0" : "1",
+    modestbranding: "1",
+    rel: "0",
+    playsinline: "1",
+    controls: controls ? "1" : "0",
+  }).toString();
+
+  const src = `https://www.youtube.com/embed/${videoId}?${params}`;
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.box} style={{ width }}> {/* OK: objeto */}
+        <div className={styles.aspect16by9}>
+          <iframe
+            className={styles.player}
+            src={src}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
